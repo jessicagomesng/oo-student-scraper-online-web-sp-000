d4e18ec9cf1e08fbaee574a8723d38d5c516359c
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-14 12:34:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,9 +9,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    
     students_array.each do |student|
       student = Student.new
-    end 
+    end
+
   end
 
   def add_student_attributes(attributes_hash)
